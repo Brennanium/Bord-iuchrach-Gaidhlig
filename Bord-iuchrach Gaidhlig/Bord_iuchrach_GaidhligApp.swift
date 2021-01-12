1a83688a3d60adfec6066918492d1b2c85abe052
Brennan Drew <nannerb156@gmail.com>
2021-01-11 21:28:54 -0800

    Initial Commit

---
+++ Bord-iuchrach Gaidhlig/Bord_iuchrach_GaidhligApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bord_iuchrach_GaidhligApp.swift
+//  Bord-iuchrach Gaidhlig
+//
+//  Created by Brennan Drew on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Bord_iuchrach_GaidhligApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
