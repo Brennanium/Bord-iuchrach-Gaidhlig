ccdc60882258acdbc8ff44cc8209450e4d10b6d7
Brennan Drew <nannerb156@gmail.com>
2021-01-18 01:33:11 -0800

    Add font weight and return key text

--- Shared/DemoKeyboardAppearanceProvider.swift
+++ Shared/DemoKeyboardAppearanceProvider.swift
@@ -8,6 +8,7 @@
 
 import KeyboardKit
 import UIKit
+import SwiftUI
 
 /**
  This provider inherits `StandardKeyboardAppearanceProvider`
@@ -22,11 +23,37 @@ class DemoKeyboardAppearanceProvider: StandardKeyboardAppearanceProvider {
         }
     }
     
+    override func fontWeight(for action: KeyboardAction, context: KeyboardContext) -> UIFont.Weight? {
+        switch action {
+        case .newLine: return .regular
+        default:
+            let hasImage = action.standardButtonImage(for: context) != nil
+            return hasImage ? .light : nil
+        }
+    }
+    
     override func text(for action: KeyboardAction) -> String? {
         switch action {
         case .space: return "falamh"
         case .newLine: return "tilleadh"
+        //case .backspace: return "\u{232B}" //delete symbol
         default: return super.text(for: action)
+        }
+    }
+    
+    func returnText(proxy: UITextDocumentProxy) -> String? {
+        let type = proxy.returnKeyType
+        switch type {
+        case .go: return "dol"
+        case .google: return "googleadh"
+        case .join: return nil
+        case .next: return "ath"
+        case .route: return "rùtachadh"
+        case .search: return "sireadh"
+        case .send: return "cur"
+        case .done: return "ullamh"
+        case .`continue` : return "buanachadh"
+        default: return "tilleadh"
         }
     }
 }
